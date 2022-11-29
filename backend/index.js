e6256055f1b4e35f48c9b986ada6c55c3b5c6307
Nishaojha <its.nishaojha@gmail.com>
2022-11-29 18:07:11 +0530

    storing data in database using Mongoose model

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())//if we want to use req.body this middleware is required
+
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
+// create a User using POST "/api/auth". doesn't require Auth
 router.get('/', (req, res)=>{
-   
-    res.json([])
+    console.log(req.body)
+    const user = User(req.body); // to create a new user
+    user.save() 
+    res.send(req.body)  
 })
 
 module.exports = router;
