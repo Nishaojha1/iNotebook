f24d6edb2c1b0a3c6b08ca92f09e879b09a5d6b4
Nishaojha <its.nishaojha@gmail.com>
2022-12-06 11:43:53 +0530

    Adding Font Awsome items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Test from "./components/Test";
 import Home from "./components/Home";
 import NoteState from "./context/notes/NoteState";
 import About from "./components/About";
+import Alert from "./components/Alert";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <NoteState>
         <BrowserRouter>
           <Navbar />
+          <Alert message="This is a react course"/>
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
