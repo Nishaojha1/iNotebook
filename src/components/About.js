5e83c4cc2cc1087d21904d69d0a5518b48fac38f
Nishaojha <its.nishaojha@gmail.com>
2022-12-06 09:40:59 +0530

    Added use Location hook in our react App

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,9 @@ import React, {useContext, useEffect} from 'react';
 import noteContext from '../context/notes/noteContext';
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update();
-  }, [])
-  
   return (
     <div>
-      This is About {a.state.name} and studying in class {a.state.class}
+      This is About page
     </div>
   )
 }
