de9e703c7ea7fb1aa7b6f38d9538293759755b25
Nishaojha <its.nishaojha@gmail.com>
2022-12-06 11:23:26 +0530

    Created noteitem

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
+import Noteitem from "./Noteitem";
 
 
 const Notes = () => {
   const context = useContext(noteContext);
   const { notes, setNotes } = context;
   return (
-    <div className="container">
+    <div className="row my-3 gy-2">
       <h2>Your Notes</h2>
       {notes.map((note) => {
-        console.log(note);
+        return <Noteitem note ={note} key={note._id}/>
       })}
     </div>
   );
