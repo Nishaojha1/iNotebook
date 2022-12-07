65744a2ee248cac2ba223a0194284fad560491f1
Nishaojha <its.nishaojha@gmail.com>
2022-12-07 21:06:30 +0530

    adding Fetch notes functionality

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())//if we want to use req.body this middleware is required
 
 // Available Routes
